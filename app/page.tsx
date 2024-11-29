0a90b477a49e66a4ec8bb560b94d83f82e2e4430
Joshua <joshua.ghattas@gmail.com>
2024-11-29 09:52:53 -0600

    small change - removed border from a div

--- app/page.tsx
+++ app/page.tsx
@@ -68,7 +68,7 @@ export default function Page() {
 
 
 
-        <div className="max-w-500 border-solid border-2 border-red-600 rounded-3xl content-center flex-1">
+        <div className="max-w-500  content-center flex-1">
           <div className="border-solid border-2 border-red-600 rounded-3xl p-5 m-5">
               <p className={"text-4xl text-blue-500 animate-rainbowText" + " " + jetbrains_800weight.className}>
                         Website is currently being updated ~daily, hope to have a reasonable beta version by 12/7/24.
