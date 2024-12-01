a4bff567e1e30abfd131848155ceebe05121a0a7
Joshua <joshua.ghattas@gmail.com>
2024-12-01 07:10:08 -0600

    Made the navbar have a div around the selected page and hidden on mobile

--- ui/homepage/pfp.tsx
+++ ui/homepage/pfp.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Pfp({ size = "1em" }) {
     return (
         <div
-      className="rounded-full overflow-hidden shrink flex items-center justify-center"
+      className="rounded-full overflow-hidden flex items-center justify-center"
       style={{
         width: size,
         height: size,
--- ui/navbar.tsx
+++ ui/navbar.tsx
@@ -30,38 +30,38 @@ export default function Navbar() {
 
     return (
         <>
-            <div className='w-full flex justify-center m-5'>
-                <div className='justify-center border-solid rounded-2xl flex content-center h-9 m-5 max-w-1/3 min-w-250 p-5 border-red-500 border-2'>
-                    <div className='relative bottom-4 w-15 h-11'>
-                        <div className='flex h-11'>
+            <div className='hidden sm:flex w-full justify-center m-5 overflow-hidden'>
+                <div className='justify-center border-solid rounded-2xl min-w-500 flex content-center h-9 m-5 p-5 border-red-500 border-2'>
+                    <div className='relative bottom-4 w-15 h-14'>
+                        <div className='inline-grid grid-cols-4 gap-3 grid-rows-1'>
 
-                            <Link className='h-10 overflow-hidden' href="/">
-                                <Pfp size="2rem" />
-                            </Link>
-
-                            <div className='flex flex-row m-5 relative'>
-                                <div className='flex flex-row justify-between'>
-                                    {links.map((link) => {
-                                        
-                                        return (
-                                            
-                                            <div className='flex p-5 flex-row' key={link.name}>
-                                                <Link
-                                                    key={link.name}
-                                                    href={link.href}
-                                                    className={clsx(jetbrains_800weight.className + " " + "rounded-2xl", {
-                                                        'bg-gray-500 animate-rainbowText-fast': pathname === link.href,
-                                                    })}>
-                                                    <p className="relative bottom-9 hidden md:block">{link.name}</p>
-                                                </Link>
-                                            </div>
-
-                                            
-                                        );
-                                    })}
-                                </div>
-                                
+                            <div className='flex justify-center'>
+                                <Link className='h-8 w-8 overflow-hidden' href="/">
+                                    <Pfp size="2rem" />
+                                </Link>
                             </div>
+
+                            {links.map((link) => {
+                                            
+                                return (
+                                    
+                                    <div className='flex flex-row w-30 justify-center' key={link.name}>
+                                        <Link
+                                            key={link.name}
+                                            href={link.href}
+                                            className={clsx(jetbrains_800weight.className + " " + "p-1 rounded-2xl", {
+                                                'bg-gray-700 animate-rainbowText-fast': pathname === link.href,
+                                            })}>
+                                            <p>{link.name}</p>
+                                        </Link>
+                                    </div>
+
+                                    
+                                );
+                            })}
+
+                                
+                                
                         </div>
                     </div>
                 </div>
