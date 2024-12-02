aafa38299e9316c5de94a1c6fe5b114cddfde2a2
InternetBowser-admin <joshua@internetbowser.com>
2024-12-02 14:16:19 +0000

    Replit isn't working, ima have to debug later

--- app/changelog/page.tsx
+++ app/changelog/page.tsx
@@ -1,31 +1,36 @@
-import { JetBrains_Mono } from 'next/font/google'
+import { JetBrains_Mono } from "next/font/google";
 
 const jetbrains_400weight = JetBrains_Mono({
-    weight: '400',
-    subsets: ['latin'],
-  })
-  
-  const jetbrains_800weight = JetBrains_Mono({
-    weight: '800',
-    subsets: ['latin'],
-  })
+    weight: "400",
+    subsets: ["latin"],
+});
+
+const jetbrains_800weight = JetBrains_Mono({
+    weight: "800",
+    subsets: ["latin"],
+});
 
 export default function Page() {
     return (
         <>
             <main>
-                <div className='flex justify-center items-center'>
+                <div className="flex justify-center items-center">
                     <div>
-                        <h1 className={"text-3xl" + " " + jetbrains_800weight.className}>
+                        <h1
+                            className={
+                                "text-3xl" + " " + jetbrains_800weight.className
+                            }
+                        >
                             This page is under construction
                         </h1>
                         <br />
                         <p className={"" + " " + jetbrains_400weight.className}>
-                            This feature is next on my todo list, don&#39;t worry.
+                            This feature is next on mytodo list, don&#39;t
+                            worry.
                         </p>
-                    </div> 
+                    </div>
                 </div>
             </main>
         </>
-    )
+    );
 }
