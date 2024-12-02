03fc609fd309172228d69d3020cb620308bed08c
Joshua <joshua.ghattas@gmail.com>
2024-12-01 21:01:47 -0600

    added og img

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,11 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="google-adsense-account" content="ca-pub-7456236988733866" />
+        <meta property="og:image" content="<generated>" />
+        <meta property="og:image:type" content="<generated>" />
+        <meta property="og:image:width" content="<generated>" />
+        <meta property="og:image:height" content="<generated>" />
+
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7456236988733866"
       crossOrigin="anonymous"></script>
       </head>
