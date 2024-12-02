4fd948b2952f31317b04382af962644e40a27529
InternetBowser-admin <joshua@internetbowser.com>
2024-12-02 16:17:40 +0000

    actually fixed the linter

--- app/version/page.tsx
+++ app/version/page.tsx
@@ -1,14 +1,7 @@
 import VersionName from "@/ui/version-name";
-import { JetBrains_Mono } from "next/font/google";
-
-const jetbrains_400weight = JetBrains_Mono({
-  weight: "400",
-  subsets: ["latin"],
-});
-
 export default function Page() {
   return (
-    <div className="flex w-full jetbrains_400weight.classname text-white h-full flex justify-center content-center">
+    <div className="flex w-full  text-white h-full flex justify-center content-center">
       <VersionName />
     </div>
   );
