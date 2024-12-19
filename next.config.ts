4e718d407b56f41a39fbcaad88e80d78fee0303d
Joshua <joshua.ghattas@gmail.com>
2024-12-18 19:09:32 -0600

    Adjusted webpack

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
+const path = require('path');
 
 const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'app');
+    return config;
+  },
   async rewrites() {
     return [
       {
