551daec9011ba811de2015e729a04739539520ec
Joshua <joshua.ghattas@gmail.com>
2024-12-22 21:21:45 -0600

    Fixed an error with the middleware

--- middleware.ts
+++ middleware.ts
@@ -11,17 +11,3 @@ export const config = {
     '/(api|trpc)(.*)',
   ],
 }
-
-export function middleware(req: NextRequest) {
-  const res = NextResponse.next();
-
-  res.headers.set("Access-Control-Allow-Origin", "*");
-  res.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-  if (req.method === "OPTIONS") {
-    return new Response(null, { status: 200 });
-  }
-
-  return res;
-}
--- next.config.ts
+++ next.config.ts
@@ -4,14 +4,6 @@ const path = require('path');
 const nextConfig: NextConfig = {
   async headers() {
     return [
-      {
-        source: "/api/:path*", // Match all API routes
-        headers: [
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET, POST, OPTIONS" },
-          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
-        ],
-      },
     ];
   },
   images: {
